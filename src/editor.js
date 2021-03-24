82a1308e8d04ffac000a00773535dcfa44ac0b90
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2021-03-24 07:52:15 +0100

    Wrap code in custom element
    This allows the code-editor element to be used like the code HTML
    element. It provides a better integration with simple HTML pages.

--- src/editor.js
+++ src/editor.js
@@ -1,15 +1,52 @@
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
-import {python} from "@codemirror/lang-python"
+import { EditorState } from "@codemirror/state"
+import { EditorView, keymap, highlightSpecialChars, drawSelection, highlightActiveLine } from "@codemirror/view"
+import { history, historyKeymap } from "@codemirror/history"
+import { foldKeymap } from "@codemirror/fold"
+import { indentOnInput } from "@codemirror/language"
+import { defaultKeymap } from "@codemirror/commands"
+import { bracketMatching } from "@codemirror/matchbrackets"
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets"
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
+import { autocompletion, completionKeymap } from "@codemirror/autocomplete"
+import { commentKeymap } from "@codemirror/comment"
+import { rectangularSelection } from "@codemirror/rectangular-selection"
+import { defaultHighlightStyle } from "@codemirror/highlight"
+import { lintKeymap } from "@codemirror/lint"
+import { python } from "@codemirror/lang-python"
 
 class CodeEditor extends HTMLElement {
   constructor() {
     super()
 
-    const shadow = this.attachShadow({mode: 'open'})
+    const code = this.firstElementChild.innerText
+    const shadow = this.attachShadow({ mode: 'open' })
+
     new EditorView({
       state: EditorState.create({
+        doc: code,
         extensions: [
-          basicSetup,
+          highlightSpecialChars(),
+          history(),
+          drawSelection(),
+          EditorState.allowMultipleSelections.of(true),
+          indentOnInput(),
+          defaultHighlightStyle.fallback,
+          bracketMatching(),
+          closeBrackets(),
+          autocompletion(),
+          rectangularSelection(),
+          highlightActiveLine(),
+          highlightSelectionMatches(),
+          keymap.of([
+            ...closeBracketsKeymap,
+            ...defaultKeymap,
+            ...searchKeymap,
+            ...historyKeymap,
+            ...foldKeymap,
+            ...commentKeymap,
+            ...completionKeymap,
+            ...lintKeymap
+          ]),
           python()
         ]
       }),
